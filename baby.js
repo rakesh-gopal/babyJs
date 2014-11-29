7c741f25d3875cc9b07c7eb771b0b8c2efd557cf
Rakesh P Gopal <rakesh@actmobile.com>
2014-11-30 03:25:05 +0530

    Added the baby JS file

---
+++ baby.js
@@ -0,0 +1,16 @@
+/*
+ * Author: Rakesh P Gopal (https://github.com/rakesh-gopal)
+ * Vertion: 0.1
+ * License: http://www.apache.org/licenses/
+ */
+
+var Baby = (function(){
+    var version = "0.1";
+
+    var baby = function(){
+    };
+
+    baby.version = version;
+
+    return baby;
+}());
